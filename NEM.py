0d36c47f9290558b1bbfe0411cfb558592ed9e24
Nick <n.gorman305@gmail.com>
2018-08-01 16:02:09 +1000

    grouping functrion working

--- NEM.py
+++ NEM.py
@@ -3,6 +3,7 @@ import pandas as pd
 from random import shuffle
 from fuzzywuzzy import fuzz
 import itertools
+import author_space
 
 paper_data = pd.read_csv('nem_papers.csv', dtype=str)
 
@@ -25,67 +26,7 @@ for author_list in authors:
 
 authors_and_nodes = dict(zip(paper_data['Author and Year'], lists_of_authors))
 
-def cal_goodness(order):
-    adjacent_match_count = 0
-    for element in order:
-        index = order.index(element)
-        if index == len(order) - 1:
-            break
-        next_element = order[index + 1]
-        match_count = 0
-        for name1, name2 in itertools.product(element.split(';'), next_element.split(';')):
-            temp_distance = fuzz.ratio(name1, name2)
-            if temp_distance > 80:
-                match_count += 1
-        if index < len(order) - 2:
-            next_element = order[index + 2]
-            for name1, name2 in itertools.product(element.split(';'), next_element.split(';')):
-                temp_distance = fuzz.ratio(name1, name2)
-                if temp_distance > 80:
-                    match_count += 0.5
-        adjacent_match_count += match_count
-
-    return adjacent_match_count
-
-def matches(list1, list2):
-    match_count = 0
-    for name1, name2 in itertools.product(list1.split(';'), list2.split(';')):
-        temp_distance = fuzz.ratio(name1, name2)
-        if temp_distance > 80:
-            match_count += 1
-    return match_count
-
-comp_list = lists_of_authors.copy()
-groups = []
-current_group = 0
-for element in comp_list:
-    comp_list[comp_list.index(element)] = ''
-    groups.append([element])
-    sub_com_list = comp_list.copy()
-    for comp_elment in sub_com_list:
-        if matches(element, comp_elment) > 0:
-            groups[current_group].append(comp_elment)
-            comp_list.pop(comp_list.index(comp_elment))
-    current_group += 1
-
-potential_orders = []
-order_goodness = []
-temp_count =0
-for i in range(1, 1000000):
-    shuffle(lists_of_authors)
-    try_order = lists_of_authors.copy()
-    potential_orders.append(try_order)
-    order_goodness.append(cal_goodness(try_order))
-    if temp_count == 10000:
-        print(i)
-        temp_count = 0
-    temp_count += 1
-
-max_distance = max(order_goodness)
-best_order = potential_orders[order_goodness.index(max_distance)]
-
-for names in best_order:
-    print(names)
+author_groups = author_space.group_author_lists(authors)
 
 edges = [('Chand, Pramesh, 2006', 'Thatcher, Marcus J., 2007'),
          ('Vytelingum, Perukrishnen, 2009', 'Chand, Pramesh, 2006'),
@@ -94,6 +35,6 @@ edges = [('Chand, Pramesh, 2006', 'Thatcher, Marcus J., 2007'),
          ('Wagner, Liam, 2010', 'Exemplar, Energy, 2000'),
          ('Hindsberger, Magnus, 2011', 'Exemplar, Energy, 2000')]
 
-network_map.create_cit_map(nodes, edges, years, authors_and_nodes, best_order)
+network_map.create_cit_map(nodes, edges, years, authors_and_nodes, 1)
 
 
--- author_space.py
+++ author_space.py
@@ -4,27 +4,47 @@ import itertools
 
 def group_author_lists(author_lists):
     groups = []
-    for index, author_list in enumerate(author_lists):
+    for index1, author_list in enumerate(author_lists):
 
-        if index == 0:
+        if index1 == 0:
             groups.append([author_list])
-        else:
-            if author_list in [item for sublist in groups for item in sublist]:
+        elif index1 + 1 == len(author_lists):
                 continue
 
-        all_authors_in_group = [authors.split(';') for authors in groups[-1]]
+        found_groups = []
 
-        if matching_authors(all_authors_in_group, author_list[index + 1]) > 0:
-            groups[-1].append(author_list[index + 1])
+        for index2, group in enumerate(groups):
+            all_authors_in_group = [authors.split(';') for authors in group]
+            all_authors_in_group = [item for sublist in all_authors_in_group for item in sublist]
+
+            if matching_authors(all_authors_in_group, author_lists[index1 + 1].split(';')) > 0:
+                found_groups.append(index2)
+
+        if len(found_groups) == 0:
+            groups.append([author_lists[index1 + 1]])
         else:
-            groups.append([author_list])
+            new_group = []
+            for index3 in sorted(found_groups, reverse=True):
+                for element in groups[index3]:
+                    new_group.append(element)
+                del groups[index3]
+            new_group.append(author_lists[index1 + 1])
+            groups.append(new_group)
+
     return groups
 
 
 def matching_authors(list1, list2):
     match_count = 0
     for name1, name2 in itertools.product(list1, list2):
-        temp_distance = fuzz.ratio(name1, name2)
-        if temp_distance > 80:
+        names1 = name1.split(',')
+        last_name_1 = names1[0]
+        names2 = name2.split(',')
+        last_name_2 = names2[0]
+        first_name_1 = names1[-1].strip().split(' ')[0]
+        first_name_2 = names2[-1].strip().split(' ')[0]
+        temp_distance_last = fuzz.ratio(last_name_1.lower().strip(), last_name_2.lower().strip())
+        temp_distance_first = fuzz.ratio(first_name_1.lower().strip(), first_name_2.lower().strip())
+        if temp_distance_last > 80 and temp_distance_first > 80:
             match_count += 1
     return match_count
